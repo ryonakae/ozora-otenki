b861cb54ec1392319140d0b4c908f2629e3b8f1f
Ryo Nakae <ryo.n.1129@gmail.com>
2015-04-10 15:11:05 +0900

    Livedoor Weather Hacks APIで天気取得したり

---
+++ forecast.js
@@ -0,0 +1,59 @@
+// var express = require('express');
+// var app = express();
+// var Forecast = require('forecast');
+
+// var options = {
+//   forecast_api_key: process.env.FORECAST_API_KEY
+// };
+// app.set('options', options);
+
+// var forecast = new Forecast({
+//   service: 'forecast.io',
+//   key: app.get('options').forecast_api_key,
+//   units: 'celcius',
+//   cache: true,
+//   ttl: {
+//     minutes: 27,
+//     seconds: 45
+//   }
+// });
+
+// forecast.get([35.659117, 139.703691], function(err, weather){
+//   if(err) return console.dir(err);
+//   console.dir(weather);
+// });
+
+
+// weather = require('openweathermap');
+
+// weather.defaults({
+//   units: 'metric',
+//   lang: 'en',
+//   mode: 'json'
+// });
+
+// weather.daily({
+//   id: 1850147,
+//   cnt: 3
+// }, function(err, json){
+//   console.dir(json);
+// });
+
+
+var http = require('http');
+var url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010';
+http.get(url, function(res){
+    var body = '';
+    res.setEncoding('utf8');
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(res){
+        ret = JSON.parse(body);
+        console.log(ret);
+    });
+}).on('error', function(e){
+    console.log(e.message); //エラー時
+});
