b8be6ecf4cc42ce0eedb12cfe85f6f3ab551df8f
Ryo Nakae <ryo.n.1129@gmail.com>
2015-04-13 19:42:27 +0900

    Viewちゃんと表示されるようにした

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-// 環境変数から Titter アプリケーションのキー等を取得
+// ozora-otenki options
 var options = {
   twitter_key: process.env.TWITTER_KEY,
   twitter_secret: process.env.TWITTER_SECRET,
   twitter_token: process.env.TWITTER_TOKEN,
-  twitter_token_secret: process.env.TWITTER_TOKEN_SECRET,
-  test: process.env.TEST
+  twitter_token_secret: process.env.TWITTER_TOKEN_SECRET
 };
 app.set('options', options);
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
-  response.send('Ozora Otenki')
+  response.render('index', { title:'Ozora Otenki' })
 });
 
 app.listen(app.get('port'), function() {
