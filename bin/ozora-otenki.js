b9cbbc1e14e525e38a594a92f94555d5fc397187
Ryo Nakae <ryo.n.1129@gmail.com>
2015-04-13 16:52:19 +0900

    毎朝8時半にあかりちゃんが天気教えてくれる

--- bin/ozora-otenki.js
+++ bin/ozora-otenki.js
@@ -5,6 +5,7 @@ var moment = require('moment');
 var weather = require('openweathermap');
 var http = require('http');
 var async = require('async');
+var fs = require('fs');
 require('date-utils');
 
 
@@ -15,12 +16,16 @@ var twitter = new twit({
   access_token_secret: app.get('options').token_secret
 });
 
+// openweathermap
 var cityId = 1850147;
+// livedoor weather hacks
 var url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=130010';
 
-// var cronTime = '0 30 8 * * *'; //毎日朝8時半
-// var cronTime = '30 * * * * *'; //30秒毎
-var cronTime = '0 */5 * * * *'; //5分毎
+// 秒 分 時 日 月 週
+var cronTime = '0 30 8 * * *'; //毎日朝8時半
+// var cronTime = '1-59/2 * * * *'; //1-59分の間で2分ごと
+// var cronTime = '*/15 * * * *'; //15分ごと
+
 
 new cronJob({
   cronTime: cronTime,
@@ -30,6 +35,7 @@ new cronJob({
   start: true,
   timeZone: 'Asia/Tokyo'
 });
+
 
 function tweet(){
   async.waterfall([
@@ -44,8 +50,16 @@ function tweet(){
         id: cityId,
         cnt: 2
       }, function(err, result){
-        var temp_max = Math.round(result.list[0].temp.max);
-        var temp_min = Math.round(result.list[0].temp.min);
+        if(result.list[0].temp.max != null){
+          var temp_max = Math.round(result.list[0].temp.max);
+        } else {
+          var temp_max = '-';
+        }
+        if(result.list[0].temp.min != null){
+          var temp_min = Math.round(result.list[0].temp.min);
+        } else {
+          var temp_min = '-';
+        }
         callback(null, temp_max, temp_min);
       });
     },
@@ -59,25 +73,54 @@ function tweet(){
         });
         res.on('end', function(res){
           result = JSON.parse(body);
-          var telop = result.forecasts[0].telop;
+          if(result.forecasts[0].telop != null){
+            var telop = result.forecasts[0].telop;
+          } else {
+            var telop = '-';
+          }
           callback(null, temp_max, temp_min, telop);
         });
       });
     },
-    // output
+    // tweet
     function(temp_max, temp_min, telop){
       var d = new Date();
       var time = d.toFormat("HH24時MI分");
 
+      // ツイート文言
       var text =
-        '時刻は' + time + '\u2757 \n' +
-        '今日のお空はどんな空〜\u2753 大空お天気の時間です\u2728 \n' +
-        '今日の都心部は' + telop + '\u2757 ️\n' +
-        '最高気温は' + temp_max + '度、' + '最低気温は' + temp_min + '度です\u2b50 ️\n' +
-        '通勤・通学気をつけて、行ってらっしゃ〜い\u2728';
+        '時刻は' + time + '\u2757 ' + '今日のお空はどんな空〜\u2753 大空お天気の時間です\u2728\n' +
+        '今日の都心部は' + telop + '\u2757️ ' + '最高気温は' + temp_max + '度、' + '最低気温は' + temp_min + '度です\u2b50\n' +
+        '通勤・通学気をつけて、行ってらっしゃ〜い\u2728️';
 
-      twitter.post('statuses/update', { status: text }, function(err, data, response) {
-        console.log('tweet done.');
+      // 添付する画像
+      var imagePath = './source/images/'; // プロジェクトルートからの相対パス
+      var imageCount = 11; // フォルダ内の画像の総数
+      var imageNum = Math.floor(Math.random() * imageCount) + 1;
+
+      if(imageNum < 10){
+        imageNum = '00' + imageNum;
+      }
+      else if(10 <= imageNum <= 99){
+        imageNum = '0' + imageNum;
+      }
+
+      // 画像をbase64エンコードしてStreamに追加
+      var image = fs.readFileSync('./source/images/' + imageNum + '.jpg', { encoding:'base64' });
+
+      // 画像をアップロード
+      twitter.post('media/upload', { media: image }, function(err, data, response) {
+        // アップロードした画像の情報を取得
+        var mediaId = data.media_id_string;
+        var tweetContent = {
+          status: text,
+          media_ids: [mediaId]
+        };
+
+        // 文言を画像と一緒にポスト
+        twitter.post('statuses/update', tweetContent, function(err, data, response) {
+          console.log('投稿完了！おつか〜');
+        });
       });
     }
   ]);
