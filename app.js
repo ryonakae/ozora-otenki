f8fcbfbc5b95e6f0647a311fdb5236221c4cfb0e
Ryo Nakae <ryo.n.1129@gmail.com>
2015-04-13 17:38:28 +0900

    update

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ var options = {
 };
 app.set('options', options);
 
+app.set('port', (process.env.PORT || 3000));
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.get('/', function (req, res) {
   res.render('index', { title : 'Ozora Otenki' });
 });
-app.listen(3000);
+app.listen(app.get('port'), function(){
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 module.exports = app;
